c01d2ac5a6819df13ee1fbed237066fbd4eb45a0
Insurgentko <123051300+Insurgentko@users.noreply.github.com>
2024-03-11 14:41:15 +0200

    make based UI, and add strings no strings.xml

--- app/src/main/java/ua/edu/lntu/olexandr_zaitsev_cw1_perezdacha/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/olexandr_zaitsev_cw1_perezdacha/MainActivity.kt
@@ -1,15 +1,31 @@
 package ua.edu.lntu.olexandr_zaitsev_cw1_perezdacha
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import ua.edu.lntu.olexandr_zaitsev_cw1_perezdacha.ui.theme.Olexandr_Zaitsev_cw1_perezdachaTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +38,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -30,17 +46,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Greeting( modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxWidth()
+        .height(150.dp),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = painterResource(R.drawable.android_logo), contentDescription = "Android logo"
+        )
+        Text(
+            text = stringResource(R.string.full_name), fontSize = 24.sp
+        )
+        Text(text = stringResource(R.string.title), fontSize = 16.sp)
+    }
+    Spacer(modifier = Modifier.height(16.dp))
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Bottom){
+    Row() {
+        Text(text = stringResource(R.string.number), fontSize = 16.sp)
+    }
+        Row() {
+            Text(text = stringResource(R.string.more), fontSize = 16.sp)
+        }
+        Row() {
+            Text(text = stringResource(R.string.email), fontSize = 16.sp)
+        }
+}
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Olexandr_Zaitsev_cw1_perezdachaTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
